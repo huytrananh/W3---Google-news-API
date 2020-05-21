5792a5ea6d853a9a3e6b78f759607e00fe18c4b3
huyanhtran <trananhhuy1991@gmail.com>
2020-05-21 07:04:16 +0700

    fix UI

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let newsList = []
 const apiKey = "e1761270d6584b7f8443dae877e60dd9"
 
 const loadNews = async() => {
-    let url = `https://newsapi.org/v2/everything?q=motoGP&page=${page}&sortBy=publishedAt&language=en&apiKey=${apiKey}`
+    let url = `https://newsapi.org/v2/everything?q=MotoGP&page=${page}&sortBy=publishedAt&language=en&apiKey=${apiKey}`
     let data = await fetch(url)
     let result = await data.json()
     let dataList = result.articles
@@ -17,18 +17,19 @@ const render = (list) => {
     <div id="news" class="row">
         <div id="contentsArea" class="col-8">
             <div id="title">${item.title}</div>
+            <div id="publishedAt">${publicTime(item.publishedAt)}</div>
+            <div id="author">${item.author}</div>
             <div id="description">${item.description}</div>
             <div id="source"><a href="#">${item.source.name}</a></div>
-            <div id="publishedAt">${publicTime(item.publishedAt)}</div>
         </div>
         <div id="imgArea" class="col-4" style="text-align: center">
-            <img src="${item.urlToImage}" style="width: inherit" alt="">
+            <img src="${item.urlToImage}" style="width: inherit; height: 250px" alt="">
         </div>
     </div>
 `).join("")
 
     document.getElementById("newsArea").innerHTML = newsHtml
-    document.getElementById("number-news").innerHTML = newsList.length
+    document.getElementById("number-news").innerHTML = `Numbers of Articles: ${newsList.length}`
 }
 
 function publicTime(time) {     
